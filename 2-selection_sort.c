666781820d6817effa0c2d2bc0ed9576376498d2
Imukua <ianmukuaa@gmail.com>
2023-06-24 17:45:19 +0300

    added check fro change of min_idx

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -30,7 +30,7 @@ void selection_sort(int *array, size_t size)
 		return;
 	}
 
-	for (i = 0; i < size-1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
@@ -40,7 +40,10 @@ void selection_sort(int *array, size_t size)
 				min_idx = j;
 			}
 		}
-		swap(&array[i], &array[min_idx]);
-		print_array(array, size);
+		if (min_idx != (int)i)
+		{
+			swap(&array[i], &array[min_idx]);
+			print_array(array, size);
+		}
 	}
 }
