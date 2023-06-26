4840935222df7a3f5d7267b0045fe790c2aa4490
Imukua <ianmukuaa@gmail.com>
2023-06-26 17:18:11 +0300

    updated prototypes for 3-quicksort

--- sort.h
+++ sort.h
@@ -21,9 +21,17 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
+/*Other helping functions*/
+void swap(int *a, int *b);
+void swap_nodes(listint_t **list, listint_t **node1, listint_t *node2);
+int partition(int *array, int low, int high); 
+
+
+
 /*Prototypes of Sorting algos prototypes*/
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
 
 #endif
